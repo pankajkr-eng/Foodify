6f1fe903bcbe638d3ee1fb1ad85235241d155a20
Pankaj kr <pankajkmr9931@gmail.com>
2025-01-26 16:32:49 +0530

    build issue resolved

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
-import Shimmer from "./shimmer";
+import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
